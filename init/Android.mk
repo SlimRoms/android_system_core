fbd15c89d65ddd1e1fb08bfc1e7fa28de8ad6165
Chris Fries <cfries@motorola.com>
2013-09-05 13:19:21 -0500

    fs_mgr: introduce fs_mgr_format to format wiped partitions
    Move fastboot's format logic into fs_mgr, to consolidate the knowledge
    about how to do this (and when to wipe metadata).
    
    Try to format these formattable paritions if they are wiped.
    
    If formatting fails, we will fall out to let recovery mode handle it.
    
    Bug: 20082763
    Change-Id: I397cc197550e78d932e8a154fd234695c46dbe7b

--- init/Android.mk
+++ init/Android.mk
@@ -61,6 +61,10 @@ LOCAL_STATIC_LIBRARIES := \
 	libselinux \
 	libmincrypt \
 	libext4_utils_static \
+	libext2_uuid_static \
+	liblz4-static \
+	libsparse_static \
+	libz
 	libext2_blkid \
 	libext2_uuid
 
