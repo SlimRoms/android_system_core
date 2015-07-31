363f2c9092ab5d7249186df6a95d847a34c69f7f
William Roberts <william.c.roberts@intel.com>
2015-07-31 13:11:19 -0700

    sdcard: use libpackageparser
    Switch from the internal packages.list file parser
    implementation to a common parser library.
    
    See Change-Id: I87a406802f95d8e7bfd8ee85f723f80e9e6b6c0c
    for all of the details.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    
    Conflicts:
    	sdcard/Android.mk
    
    Change-Id: I2269e608e2651c445f3f07bd0cb1353d2ffd4816

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -10,6 +10,6 @@ LOCAL_SRC_FILES := sdcard.c
 LOCAL_MODULE := sdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter -Werror
 
-LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_SHARED_LIBRARIES := libcutils libpackagelistparser
 
 include $(BUILD_EXECUTABLE)
