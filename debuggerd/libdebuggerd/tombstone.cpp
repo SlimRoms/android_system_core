7a4d4129da4ad913ae9676abe21d947fe861083a
Scott Mertz <scott@cyngn.com>
2016-05-25 15:47:42 -0700

    debuggerd: add Slim version to tombstones
    Change-Id: I7822a2e133a2f326ad2d8da8f79b0064344d59bf
    
    debuggerd: Rebrand tombstone header to Slim
    
    [harryyoud]: Use new props for 15.1
    [rashed]: Bah... use GetProperty
    
    Change-Id: Idd08c2eb7e395b464b1510742bf52833f465db08

--- debuggerd/libdebuggerd/tombstone.cpp
+++ debuggerd/libdebuggerd/tombstone.cpp
@@ -72,7 +72,9 @@ using namespace std::literals::string_literals;
 static void dump_header_info(log_t* log) {
   auto fingerprint = GetProperty("ro.build.fingerprint", "unknown");
   auto revision = GetProperty("ro.revision", "unknown");
+  auto slim_version = GetProperty("ro.slim.version", "unknown");
 
+  _LOG(log, logtype::HEADER, "Slim Version: '%s'\n", slim_version.c_str());
   _LOG(log, logtype::HEADER, "Build fingerprint: '%s'\n", fingerprint.c_str());
   _LOG(log, logtype::HEADER, "Revision: '%s'\n", revision.c_str());
   _LOG(log, logtype::HEADER, "ABI: '%s'\n", ABI_STRING);
