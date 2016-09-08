f822f30de8ba3af20d9f46078d8d4a8ead763d2c
Nicholas Flintham <nick@flinny.org>
2016-09-08 10:08:09 +0100

    adb: Fix compilation issue
    Change-Id: Ia3284d5b8e428474726f64231f8142cf2474b17f

--- adb/services.cpp
+++ adb/services.cpp
@@ -86,9 +86,8 @@ void restart_root_service(int fd, void *cookie) {
         property_get("ro.build.type", build_type, "");
         property_get("ro.slim.version", slim_version, "");
 
-        if (strlen(slim_version) > 0 && strcmp(build_type, "eng") != 0 && (atoi(value) & 2) != 2) {
-            snprintf(buf, sizeof(buf), "root access is disabled by system setting - enable in settings -> development options\n");
-            writex(fd, buf, strlen(buf));
+        if (strlen(cm_version) > 0 && strcmp(build_type, "eng") != 0 && (atoi(value) & 2) != 2) {
+            WriteFdExactly(fd, "root access is disabled by system setting - enable in settings -> development options\n");
             adb_close(fd);
             return;
         }
